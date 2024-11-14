229db78098599bc6807f25327722c310fda0ba0b
kanjun0402 <kanjun0402@gmail.com>
2024-11-14 16:40:33 +0900

    getter/setterメソッドの廃止

--- src/Main.java
+++ src/Main.java
@@ -21,11 +21,11 @@ public class Main {
     while (true) {
       mp.drawPhase(scanner);
       mp.battlePhase();
-      if (mp.getPlayerHp() <= 0 && mp.getCpuHp() <= 0) {
+      if (mp.playerHP <= 0 && mp.cpuHP <= 0) {
         System.out.println("引き分け！");
-      } else if (mp.getPlayerHp() <= 0) {
+      } else if (mp.playerHP <= 0) {
         System.out.println("CPU Win!");
-      } else if (mp.getCpuHp() <= 0) {
+      } else if (mp.cpuHP <= 0) {
         System.out.println("Player Win!");
       } else {
         Thread.sleep(2000);
